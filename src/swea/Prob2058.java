8dfaaa78e2f28b9862a8a9e36c4d497833871981
조미정 <ing06164@naver.com>
2022-12-13 22:04:08 +0900

    Prob2058 - 자릿수 더하기 문제 해결

---
+++ src/swea/Prob2058.java
@@ -0,0 +1,23 @@
+package swea;
+
+import java.util.Scanner;
+
+/* 2058. 자릿수 더하기 */
+public class Prob2058 {
+	public static void main(String args[]) throws Exception
+	{
+    	Scanner sc = new Scanner(System.in);
+		int sumOfNum=0;			// 각 자릿수들의 합
+		
+		int numbers = Integer.parseInt(sc.next());
+			
+		// 각 자릿수의 합을 구함
+		while(numbers > 0) {
+			sumOfNum += numbers % 10;		// 일의 자리부터 각 자릿수를 추출 ex) 1234 -> 4, 3, 2, 1 순서
+			numbers = numbers/10;			// 다음 자릿수 추출을 위해 10으로 나눔
+		}
+			
+		System.out.println(sumOfNum);
+
+    }
+}
