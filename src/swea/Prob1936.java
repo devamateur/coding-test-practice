4ba0577e0602dbfb434a0a364cc3857091e64e12
조미정 <ing06164@naver.com>
2022-12-14 19:30:49 +0900

    Prob1936 - 1대 1 가위바위보 해결

---
+++ src/swea/Prob1936.java
@@ -0,0 +1,20 @@
+package swea;
+
+import java.util.Scanner;
+
+/* 1대 1 가위바위보 - 가위바위보를 숫자로 입력받고 이긴 사람 출력 */
+public class Prob1936 {
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		if(a>b)
+			System.out.println("A");
+		else if(a<b)
+			System.out.println("B");
+		
+		sc.close();
+	}
+}
