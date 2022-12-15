43e20886de01dcd5f6ce75bfe4524ed37a374864
조미정 <ing06164@naver.com>
2022-12-15 16:26:03 +0900

    Prob1933 - N의 약수 구하기 해결

---
+++ src/swea/Prob1933.java
@@ -0,0 +1,25 @@
+package swea;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/* 1933. N의 약수 */
+public class Prob1933 {
+	public static void main(String args[]) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		List<Integer> divisors = new ArrayList<>();		// 약수
+		
+		for(int i=1; i<=n; i++) {
+			if(n%i == 0) {		// 나머지가 0이면 약수
+				divisors.add(i);
+			}
+		}
+
+		for(Integer divisor: divisors) {
+			System.out.print(divisor+" ");
+		}
+	}
+}
