01d924b031b8fb23c08e8ef919c3cba603264119
조미정 <ing06164@naver.com>
2022-12-15 16:12:16 +0900

    Prob2068 - 최대수 구하기 해결

---
+++ src/swea/Prob2068.java
@@ -0,0 +1,30 @@
+package swea;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+/* 2068. 최대수 구하기 */
+public class Prob2068 {
+	public static void main(String args[]) throws Exception
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		
+		List<Integer> list;
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{	
+			list = new ArrayList<>();
+			for(int i=0; i<10; i++) {
+				list.add(sc.nextInt());
+			}
+			
+			Collections.sort(list);			// 리스트 정렬
+			System.out.println("#"+test_case+" "+list.get(list.size()-1));
+		}
+	}
+}
