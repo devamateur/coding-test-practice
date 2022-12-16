3c97fcdf09c6d51564d9caefcdbbeb3bbf6f4c5a
조미정 <ing06164@naver.com>
2022-12-16 17:51:51 +0900

    Prob2025 - 1부터 주어진 숫자까지의 합 출력하는 문제

---
+++ src/swea/Prob2025.java
@@ -0,0 +1,20 @@
+package swea;
+
+import java.util.Scanner;
+
+/* 2025. N줄덧셈 - 1부터 주어진 숫자만큼 모두 더한 값을 출력 */
+public class Prob2025 {
+	public static void main(String args[]) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		
+		int sum = 0;
+		int num = sc.nextInt();
+		
+		for(int i=0; i<num; i++) {
+			sum += (i+1);
+		}
+		System.out.println(sum);
+		
+		sc.close();
+	}
+}
