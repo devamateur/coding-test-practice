74185444df01339d128ef72d75c02c943be0fda5
조미정 <ing06164@naver.com>
2022-12-17 14:42:06 +0900

    Prob2027 - 대각선 출력하기 해결

---
+++ src/swea/Prob2027.java
@@ -0,0 +1,16 @@
+package swea;
+
+/* 2027. 대각선 출력하기 */
+public class Prob2027 {
+	public static void main(String args[]) throws Exception {
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<5; j++) {
+				if(i==j)
+					System.out.print("#");
+				else
+					System.out.print("+");
+			}
+			System.out.println();
+		}
+	}
+}
