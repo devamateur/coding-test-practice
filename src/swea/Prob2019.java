ad8537dfd1f2f7e0f6f59f4473184096c323ca73
조미정 <ing06164@naver.com>
2022-12-18 21:56:27 +0900

    Prob2019 - 주어진 수만큼 2 곱하는 문제 해결

---
+++ src/swea/Prob2019.java
@@ -0,0 +1,24 @@
+package swea;
+
+import java.util.Scanner;
+
+/* 2019. 더블더블 */
+// 1부터 주어진 횟수까지 2를 곱한 값 출력
+public class Prob2019 {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		int number = sc.nextInt();
+		
+		int times = 1;
+		
+		System.out.print(times+" ");
+		
+		for(int i=0; i<number; i++) {
+			times *= 2;
+			System.out.print(times+" ");
+		}
+		
+		sc.close();
+	}
+}
