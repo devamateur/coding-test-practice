01d48ca0dff2f20183322c8dbf40b6b7dd1484f2
조미정 <ing06164@naver.com>
2022-12-25 17:43:03 +0900

    Prob1974 - 배열 초기화 위치 수정 후 테스트 케이스 6개 통과

--- src/swea/d2/Prob1974.java
+++ src/swea/d2/Prob1974.java
@@ -11,50 +11,56 @@ public class Prob1974 {
 		int T;
 		T=sc.nextInt();
 
-		int[][] sudoku = new int[10][10];
-		int[] rows = new int[10];		// 행에서 각 값의 중복 여부 저장(0/1)
-		int[] cols = new int[10];		// 행에서 각 값의 중복 여부 저장(0/1)
+		int[][] sudoku;
+		int[] rows;		// 행에서 각 값의 중복 여부 저장(0/1)
+		int[] cols;		// 행에서 각 값의 중복 여부 저장(0/1)
+		int[] squares;	// 3x3에서 중복 여부
 		int result = 1;
 		
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
+			sudoku = new int[10][10];
 			for(int i=0; i<9; i++) {
 				for(int j=0; j<9; j++) {
 					sudoku[i][j] = sc.nextInt();
 				}
 			}
-			
+
 			for(int i=0; i<9; i++) {
+				rows = new int[10];
+				cols = new int[10];
 				int row = 0, col = 0;
 				for(int j=0; j<9; j++) {
 					row = sudoku[i][j];
 					col = sudoku[j][i];
 					// 행
-					if(rows[row] == 1)
+					if(rows[row] == 1) {
 						result = 0;
+					}
 					// 열
-					if(cols[col] == 1)
+					if(cols[col] == 1) {
 						result = 0;
+					}
 					
 					rows[row] = 1;
 					cols[col] = 1;
 					
 					// 3x3
-					if(i%3==0 && j%3==0) {
-						int[] squares = new int[10];
+					if(i%3==0 && j%3==0) {	// 0, 3, 6
+						squares = new int[10];
 						for(int k=i; k<i+3; k++) {
 							for(int l=j; l<j+3; l++) {
 								int square = sudoku[k][l];
-								
-								if(squares[square] == 1)
+								if(squares[square] == 1) {
 									result=0;
+								}
 								
 								squares[square] = 1;
 							}
 						}
 					}
-				}			
-
+				}	
+				
 			}
 			
 			System.out.println("#"+test_case+" "+result);
