b277e3855f4935c00c6643204901c2f711ba1cdd
조미정 <ing06164@naver.com>
2022-12-25 17:20:39 +0900

    Prob1974 - 로직 구현, 결과가 모두 0인 문제 미해결

--- src/swea/d2/Prob1974.java
+++ src/swea/d2/Prob1974.java
@@ -11,9 +11,9 @@ public class Prob1974 {
 		int T;
 		T=sc.nextInt();
 
-		int[][] sudoku = new int[9][9];
-		boolean[] rows = new boolean[10];		// 행에서 각 값의 중복 여부 저장
-		boolean[] cols = new boolean[10];		// 행에서 각 값의 중복 여부 저장
+		int[][] sudoku = new int[10][10];
+		int[] rows = new int[10];		// 행에서 각 값의 중복 여부 저장(0/1)
+		int[] cols = new int[10];		// 행에서 각 값의 중복 여부 저장(0/1)
 		int result = 1;
 		
 		for(int test_case = 1; test_case <= T; test_case++)
@@ -24,16 +24,37 @@ public class Prob1974 {
 				}
 			}
 			
-			
 			for(int i=0; i<9; i++) {
-				int row, col;
+				int row = 0, col = 0;
 				for(int j=0; j<9; j++) {
 					row = sudoku[i][j];
 					col = sudoku[j][i];
+					// 행
+					if(rows[row] == 1)
+						result = 0;
+					// 열
+					if(cols[col] == 1)
+						result = 0;
 					
-					// 행, 열, 3x3 사각형 내 중복 판단...
+					rows[row] = 1;
+					cols[col] = 1;
+					
+					// 3x3
+					if(i%3==0 && j%3==0) {
+						int[] squares = new int[10];
+						for(int k=i; k<i+3; k++) {
+							for(int l=j; l<j+3; l++) {
+								int square = sudoku[k][l];
+								
+								if(squares[square] == 1)
+									result=0;
+								
+								squares[square] = 1;
+							}
+						}
+					}
+				}			
 
-				}
 			}
 			
 			System.out.println("#"+test_case+" "+result);
