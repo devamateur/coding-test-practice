05bc7873db0aafe491e1d3b7b4eb099ac1647e1b
조미정 <ing06164@naver.com>
2022-12-25 18:48:05 +0900

    Prob1974 - 메소드로 분리해서 해결

--- src/swea/d2/Prob1974.java
+++ src/swea/d2/Prob1974.java
@@ -11,61 +11,69 @@ public class Prob1974 {
 		int T;
 		T=sc.nextInt();
 
-		int[][] sudoku;
-		int[] rows;		// 행에서 각 값의 중복 여부 저장(0/1)
-		int[] cols;		// 행에서 각 값의 중복 여부 저장(0/1)
-		int[] squares;	// 3x3에서 중복 여부
+		int[][] sudoku = new int[10][10];
+
 		int result = 1;
 		
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
-			sudoku = new int[10][10];
 			for(int i=0; i<9; i++) {
 				for(int j=0; j<9; j++) {
 					sudoku[i][j] = sc.nextInt();
 				}
 			}
 
-			for(int i=0; i<9; i++) {
-				rows = new int[10];
-				cols = new int[10];
-				int row = 0, col = 0;
-				for(int j=0; j<9; j++) {
-					row = sudoku[i][j];
-					col = sudoku[j][i];
-					// 행
-					if(rows[row] == 1) {
-						result = 0;
-					}
-					// 열
-					if(cols[col] == 1) {
-						result = 0;
-					}
-					
-					rows[row] = 1;
-					cols[col] = 1;
-					
-					// 3x3
-					if(i%3==0 && j%3==0) {	// 0, 3, 6
-						squares = new int[10];
-						for(int k=i; k<i+3; k++) {
-							for(int l=j; l<j+3; l++) {
-								int square = sudoku[k][l];
-								if(squares[square] == 1) {
-									result=0;
-								}
-								
-								squares[square] = 1;
-							}
-						}
-					}
-				}	
-				
-			}
-			
+			result = getSudoku(sudoku);		// 중복 여부를 0/1로 반환하는 메소드
+		
 			System.out.println("#"+test_case+" "+result);
 		}
 		
 		sc.close();
+	}
+	
+	static int getSudoku(int[][] sudoku) {
+		int[] rows, cols;
+		for(int i=0; i<9; i++) {
+			rows = new int[10];
+			cols = new int[10];
+
+			int row, col;
+			for(int j=0; j<9; j++) {
+				row = sudoku[i][j];
+				col = sudoku[j][i];
+				
+				// 행
+				if(rows[row] == 1) {
+					return 0;
+				}
+
+				// 열
+				if(cols[col] == 1) {
+					return 0;
+				}
+				
+				rows[row] = 1;
+				cols[col] = 1;
+
+
+				// 3x3
+				if(i%3==0 && j%3==0) {	// 0, 3, 6
+					int[] squares = new int[10];
+					for(int k=i; k<i+3; k++) {
+						for(int l=j; l<j+3; l++) {
+							int square = sudoku[k][l];
+							if(squares[square] == 1) {
+								return 0;
+							}
+							
+							squares[square] = 1;
+						}
+					}
+				}
+
+			}	
+
+		}
+		return 1;
 	}
 }
