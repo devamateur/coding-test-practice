3ac8d3b04ba796ae8e7aebd0ce51350c880c33c5
조미정 <ing06164@naver.com>
2023-01-02 15:51:38 +0900

    Prob1989 - 문자열 길이가 짝수인 경우도 고려해서 회문 문제 해결

--- src/swea/d2/Prob1989.java
+++ src/swea/d2/Prob1989.java
@@ -12,19 +12,27 @@ public class Prob1989 {
 		T=sc.nextInt();
 
 		int result = 0;
+
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
 			String words = sc.next();
 			
 			int n = words.length();
 			
-			String a = words.substring(0, n/2);
-			String b = words.substring(n/2 + 1, n);
+			String a, b;
+			if(words.length() %2 == 0) {		// 문자열 길이가 짝수
+				a = words.substring(0, n/2);
+				b = words.substring(n/2, n);
+			}
+			else {								// 문자열 길이가 홀수
+				// 가운데 문자를 제외한 양 옆 문자가 같은지 비교 ex) level -> le와 el 비교
+				a = words.substring(0, n/2);
+				b = words.substring(n/2+1, n);
+			}
 			
 	        // reverse
 	        StringBuffer sb = new StringBuffer(b);
 	        String reverseB = sb.reverse().toString();			// 문자열 뒤집기
-
 			
 			if(a.equals(reverseB))
 				result = 1;
