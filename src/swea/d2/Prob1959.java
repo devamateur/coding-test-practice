14e6da58f6117ae0fdfafcdefc9700bd2ce30572
조미정 <ing06164@naver.com>
2023-01-03 20:58:54 +0900

    Prob1959 - 테케 10개 중 2개 통과

---
+++ src/swea/d2/Prob1959.java
@@ -0,0 +1,61 @@
+package swea.d2;
+
+import java.util.Scanner;
+
+public class Prob1959 {
+	public static void main(String args[]) throws Exception
+	{
+
+		Scanner sc = new Scanner(System.in);
+		int T=sc.nextInt();
+
+		int[] A, B;
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+			int N=sc.nextInt();
+			int M=sc.nextInt();
+			
+			A = new int[N];
+			B = new int[M];
+			
+			
+			for(int i=0; i<N; i++) {
+				A[i] = sc.nextInt();
+			}
+			
+			for(int i=0; i<M; i++) {
+				B[i] = sc.nextInt();
+			}
+			
+			int sum=0;
+			int max=0; 
+			if(N>M) {
+				max=0;
+				for(int i=0; i<N-M+1; i++) {
+					sum=0;
+					for(int j=0; j<M; j++) {
+						sum+=B[j]*A[i+j];
+						if(max < sum)
+							max = sum;
+					}
+				}
+			}
+			else {
+				max=0;
+				for(int i=0; i<M-N+1; i++) {
+					sum=0;
+					for(int j=0; j<N; j++) {
+						sum+=A[j]*B[i+j];
+						if(max < sum)
+							max = sum;
+					}
+				}
+			}
+			
+			System.out.println("#"+test_case+" "+max);
+
+		}
+		
+		sc.close();
+	}
+}
