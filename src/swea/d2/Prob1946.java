fe77574d8b3371b6253ece5df4cdf9fb93c85e90
조미정 <ing06164@naver.com>
2023-01-07 16:19:41 +0900

    Prob1946 - 문자열 압축을 푸는 문제 해결

--- src/swea/d2/Prob1946.java
+++ src/swea/d2/Prob1946.java
@@ -11,34 +11,35 @@ public class Prob1946 {
 		
 		for(int tc=1; tc<=T; tc++) {
 			int N = sc.nextInt();
-			
+
 			String[] alphabet = new String[N];
 			int[] alphaCount = new int[N];
-
+			
+			int totalCount = 0;
 			for(int i=0; i<N; i++) {
 				alphabet[i] = sc.next();
 				alphaCount[i] = sc.nextInt();
+				totalCount += alphaCount[i];
+			}
+
+			// 압축을 푼 문자열을 저장하기 위한 배열
+			String[] totalAlpha = new String[totalCount];
+			
+			int index = 0;
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<alphaCount[i]; j++) {
+					totalAlpha[index] = alphabet[i];		// 입력받은 수만큼 해당 알파벳들을 저장
+					index++;								// index는 alphaCount의 총합
+				}
 			}
 			
 			System.out.println("#"+tc);
-			for(int i=0; i<alphaCount.length; i++) {
-				for(int j=1; j<=alphaCount[i]; j++) {
-					if(j % 10 == 0) {
-						System.out.println();
-					}
-					else{
-						System.out.print(alphabet[i]);
-					}
+			for(int i=0; i<index; i++) {
+				if(i != 0 && i % 10 == 0) {		// 10개가 넘어가면 줄바꿈(원본 문서의 너비는 10)
+					System.out.println();
 				}
-
-				/*else {
-					for(int j=0; j<alphaCount[i]; j++) {
-						System.out.print(alphabet[i]);
-						
-					}System.out.println();
-				}*/
-				
-			}
+				System.out.print(totalAlpha[i]);
+			}System.out.println();
 		}
 		
 		sc.close();
