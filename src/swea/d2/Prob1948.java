d8d2e7d57e55651d7c6f0f4c92909d13bb0317dd
조미정 <ing06164@naver.com>
2023-01-07 16:57:14 +0900

    Prob1948 - 날짜 계산기 문제 해결

---
+++ src/swea/d2/Prob1948.java
@@ -0,0 +1,39 @@
+package swea.d2;
+
+import java.util.Scanner;
+
+public class Prob1948 {
+	public static void main(String[] args) throws Exception{
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		int[] lastDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+		for(int tc=1; tc<=T; tc++) {
+			int firstMonth = sc.nextInt();
+			int firstDay = sc.nextInt();
+			
+			int secondMonth = sc.nextInt();
+			int secondDay = sc.nextInt();
+			
+			int result=0;
+			
+			// 월이 같은 경우
+			if(firstMonth == secondMonth) {
+				result = secondDay - firstDay + 1;
+			}
+			else {
+				result += lastDays[firstMonth-1]-firstDay;
+				for(int i=firstMonth; i<secondMonth-1; i++) {
+					result += lastDays[i];
+				}
+				result += secondDay+1;
+			}
+			
+			System.out.println("#"+tc+" "+result);
+			
+		}
+		
+		sc.close();
+	}
+}
