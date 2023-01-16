195a1866a8971cfe1d008e6b1713ecd2fb1eeddf
조미정 <ing06164@naver.com>
2023-01-16 19:47:46 +0900

    Prob1244 - dfs 시간초과

--- src/swea/d3/Prob1244.java
+++ src/swea/d3/Prob1244.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 /* 1244. [S/W 문제해결 응용] 2일차 - 최대 상금 */
 public class Prob1244 {
+	static int result;
+	//static char[] num;
 	public static void main(String[] args) throws Exception{
 		Scanner sc = new Scanner(System.in);
 		
@@ -14,27 +16,38 @@ public class Prob1244 {
 			int change = sc.nextInt();
 			
 			char[] num = N.toCharArray();
+
+			//int count = 0;
+			result = 0;
+			dfs(change, num);
 			
-			for(int i=0; i<change; i++) {
-				for(int j=0; j<num.length/2; j++) {
-					for(int k=num.length-1; k>=j+1; k--) {
-						//char front = N.charAt(j);
-						//char back = N.charAt(k);
-						if(num[j] < num[k]) {
-							// 교환
-							char temp = num[j];
-							num[j] = num[k];
-							num[k] = temp;
-						}
-					}
-				}
-			}
 			System.out.print("#"+tc+" ");
-			for(int i=0; i<num.length; i++) {
-				System.out.print(num[i]);
-			}System.out.println();
+			System.out.println(result);
 		}
 		
 		sc.close();
+	}
+	static void dfs(int count, char[] num) {
+		if(count == 0) {
+			String numbers="";
+			for(int i=0; i<num.length; i++) {
+				numbers += num[i];
+			}
+			result = Math.max(result, Integer.parseInt(numbers));
+			return;
+		}
+		
+		char temp;
+		
+		for(int i=0; i<num.length; i++) {
+			for(int j=i+1; j<num.length; j++) {
+				temp = num[i];
+				num[i] = num[j];
+				num[j] = temp;
+				
+				dfs(count-1, num);
+
+			}
+		}	
 	}
 }
