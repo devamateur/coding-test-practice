4391f6a7a4a04971e90ec959843eb20e425a013c
조미정 <ing06164@naver.com>
2023-01-18 16:37:34 +0900

    Prob1244 - dfs로 최대 상금 문제 해결

--- src/swea/d3/Prob1244.java
+++ src/swea/d3/Prob1244.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 /* 1244. [S/W 문제해결 응용] 2일차 - 최대 상금 */
 public class Prob1244 {
 	static int result;
+	static char[] num;
 	public static void main(String[] args) throws Exception{
 		Scanner sc = new Scanner(System.in);
 		
@@ -14,36 +15,47 @@ public class Prob1244 {
 			String N = sc.next();
 			int change = sc.nextInt();
 			
-			char[] num = N.toCharArray();
+			num = N.toCharArray();
 
 			result = 0;
-			dfs(change, num);
+			dfs(change, 0);
 			
 			System.out.println("#"+tc+" "+result);
 		}
 		
 		sc.close();
 	}
-	static void dfs(int count, char[] num) {
-		if(count == 0) {
-			String numbers="";
+	static void dfs(int count, int index) {
+		String numbers="";
+		if(count == 0) {		// 종료 조건	
 			for(int i=0; i<num.length; i++) {
-				numbers += num[i];
+				numbers += Character.toString(num[i]);
 			}
 			result = Math.max(result, Integer.parseInt(numbers));
 			return;
 		}
 		
+		if(count % 2 == 0) {		// 남은 교환 횟수가 짝수일 때
+			for(int i=0; i<num.length; i++) {
+				numbers += Character.toString(num[i]);
+			}
+			result = Math.max(result, Integer.parseInt(numbers));
+		}
+		
 		char temp;
 		
-		for(int i=0; i<num.length; i++) {
+		for(int i=index; i<num.length; i++) {
 			for(int j=i+1; j<num.length; j++) {
-				if(num[i] <= num[j]) {
+				if(num[i] <= num[j] || count%2 == 1) {
 					temp = num[i];
 					num[i] = num[j];
 					num[j] = temp;
 					
-					dfs(count-1, num);
+					dfs(count-1, i);
+					
+					temp = num[i];
+					num[i] = num[j];
+					num[j] = temp;	
 				}
 			}
 		}	
