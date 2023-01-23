b399c6332bfaf798b4eee2b68f745b3d4f932349
조미정 <ing06164@naver.com>
2023-01-23 16:49:18 +0900

    Prob1215 - 인덱스 수정해서 문제 해결

--- src/swea/d3/Prob1215.java
+++ src/swea/d3/Prob1215.java
@@ -1,48 +1,58 @@
 package swea.d3;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
 public class Prob1215 {
+	final static int N = 8;
 	public static void main(String[] args) throws Exception{
 		Scanner sc = new Scanner(System.in);
 		
 		for(int tc=1; tc<=10; tc++) {
 			int length = sc.nextInt();		// 찾아야 하는 단어의 길이
 			
-			String[][] words = new String[8][8];
+			String[][] words = new String[N][N];
 
-			for(int i=0; i<8; i++) {
+			for(int i=0; i<N; i++) {
 				words[i] = sc.next().split("");
 			}
-			
-			List<String>[] list = new ArrayList[8];
-			List<String>[] reversed = new ArrayList[8];		// 뒤집은 단어 리스트
-			
-			for(int i=0; i<8; i++) {
-				list[i] = new ArrayList<>();
-				reversed[i] = new ArrayList<>(); 
-			}
-			
-			for(int i=0; i<8; i++) {
-				for(int j=0; j<8; j++) {
-					list[i].add(words[i][j]);
-					reversed[i].add(words[i][j]);
-				}
-			}
-			
-			// 리스트 뒤집기
-			for(int i=0; i<8; i++) {
-				Collections.reverse(reversed[i]);
-			}
-			
+
 			int count=0;
 
-			for(int i=0; i<8; i++) {
-				for(int j=0; j<8-length; j++) {
-					if(list[i].get(j) == reversed[i].get(j)) {
+			// 가로
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N-length+1; j++) {
+					boolean isPalindrome = false;
+
+					for(int k=0; k<length/2; k++) {
+						if(words[i][j+k].equals(words[i][j+length-k-1])) {
+							isPalindrome = true;
+						}
+						else {
+							isPalindrome = false;
+							break;
+						}
+					}
+					if(isPalindrome) {
+						count++;
+					}
+				}
+			}
+
+			// 세로
+			for(int i=0; i<N; i++) {
+				for(int j=0; j<N-length+1; j++) {
+					boolean isPalindrome = false;
+					
+					for(int k=0; k<length/2; k++) {
+						if(words[j+k][i].equals(words[j+length-k-1][i])) {
+							isPalindrome = true;
+						}
+						else {
+							isPalindrome = false;
+							break;
+						}
+					}
+					if(isPalindrome) {
 						count++;
 					}
 				}
