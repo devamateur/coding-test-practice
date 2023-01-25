462625f80368ba86dbe275b4546d0d3aca80a945
Mi Jeong Jo <49390382+devamateur@users.noreply.github.com>
2023-01-25 18:07:09 +0900

    [Bronze V] Title: 구구단, Time: 268 ms, Memory: 19940 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        
+        for(int i=0; i<9; i++){
+            System.out.println(n + " * " + (i+1) + " = " + n*(i+1));
+        }
+    }
+}
