cc23028fd2313ff38c919e97b1201375fd219633
Mi Jeong Jo <49390382+devamateur@users.noreply.github.com>
2023-01-25 18:08:49 +0900

    [Bronze II] Title: 블랙잭, Time: 132 ms, Memory: 14644 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int[] arr = new int[n];
+
+		st = new StringTokenizer(br.readLine(), " ");
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int result = 0;
+		
+		for(int i=0; i<n-2; i++) {
+			for(int j=i+1; j<n-1; j++) {
+				for(int k=j+1; k<n; k++) {
+					int sum = arr[i] + arr[j] + arr[k];
+					if(result < sum && sum <= m)
+						result = sum;
+				}
+			}
+		}
+		
+		bw.write(String.valueOf(result));
+		
+		br.close();
+		bw.flush();
+		bw.close();
+	}
+}
