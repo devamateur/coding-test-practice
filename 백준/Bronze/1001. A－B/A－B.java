f9cc2ced846b3010b0b3bd0f1f237662192c1c25
Mi Jeong Jo <49390382+devamateur@users.noreply.github.com>
2023-01-25 17:53:05 +0900

    [Bronze V] Title: A-B, Time: 228 ms, Memory: 18304 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        
+        int x, y;
+        x = s.nextInt();
+        y = s.nextInt();
+        System.out.println(x-y);
+    }
+}
