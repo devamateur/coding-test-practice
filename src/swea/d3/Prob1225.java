e6478375fef1f4a8fcf0c63145f5512cca3218df
조미정 <ing06164@naver.com>
2023-02-03 15:25:49 +0900

    Prob1225 큐를 이용한 방법

--- src/swea/d3/Prob1225.java
+++ src/swea/d3/Prob1225.java
@@ -1,5 +1,7 @@
 package swea.d3;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 /* 1225. [S/W 문제해결 기본] 7일차 - 암호생성기 */
@@ -7,32 +9,26 @@ public class Prob1225 {
 	public static void main(String[] args) throws Exception{
 		Scanner sc = new Scanner(System.in);
 		
+		
 		for(int tc=1; tc<=10; tc++) {
 			int T = sc.nextInt();
 
-			int[] numbers = new int[8];
+			Queue<Integer> numbers = new LinkedList<>();
 			for(int i=0; i<8; i++) {
-				numbers[i] = sc.nextInt();
+				numbers.add(sc.nextInt());
 			}
 
 			int count=1;
 			while(true) {
-				int front = numbers[0];		
+				int front = numbers.poll();		// 큐에서 맨 앞 원소를 제거
 				
 				front -= count;
 				if(front <= 0) {
-					for(int i=1; i<8; i++) {
-						numbers[i-1] = numbers[i];
-					}
-					numbers[7] = 0;		
+					numbers.offer(0);		// offer(): 큐 맨 뒤에 값 추가, add()와 같은 역할이지만 에러 발생이 X
 					break;
 				}
 				
-				for(int i=1; i<8; i++) {
-					numbers[i-1] = numbers[i];
-				}
-				
-				numbers[7] = front;
+				numbers.offer(front);
 				count++;
 				if(count == 6) {
 					count=1;
@@ -41,7 +37,7 @@ public class Prob1225 {
 			
 			System.out.print("#"+tc+" ");
 			for(int i=0; i<8; i++) {
-				System.out.print(numbers[i]+" ");
+				System.out.print(numbers.poll()+" ");
 			}System.out.println();
 
 		}
