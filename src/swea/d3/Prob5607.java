1e08d65715a23ed84f4bdd92aa840d685074ba26
조미정 <ing06164@naver.com>
2023-02-14 18:03:44 +0900

    Prob5607 초기화까지

--- src/swea/d3/Prob5607.java
+++ src/swea/d3/Prob5607.java
@@ -4,29 +4,26 @@ import java.util.Scanner;
 
 /* 5607. [Professional] 조합 */
 public class Prob5607 {
+	final static int MOD = 1234567891;
 	public static void main(String args[]) throws Exception
 	{
 
 		Scanner sc = new Scanner(System.in);
 		int T=sc.nextInt();
 
+		long[] factorial = new long[1000001];		// 팩토리얼
+		factorial[0] = 1;
+		
+		for(int i=1; i<1000001; i++) {
+			factorial[i] = (factorial[i-1]*i)%MOD;
+		}
+		
 		for(int tc = 1; tc <= T; tc++)
 		{
 			int N = sc.nextInt();
 			int R = sc.nextInt();
 			
-			int num1 = 1;
-			int num2 = 1;
-				
-			for(int i=N; i>N-R; i--) {
-				num1 *= i;
-			}
-				
-			for(int i=R; i>=1; i--) {
-				num2 *= i;
-			}
-			
-			System.out.println("#"+tc+" "+num1/num2);
+			// 페르마의 소정리 이용
 
 		}
 		
