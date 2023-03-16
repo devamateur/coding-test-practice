602a78391c38367b102e27206668570f5ca4f3fb
Mi Jeong Jo <49390382+devamateur@users.noreply.github.com>
2023-03-16 19:13:42 +0900

    [Bronze II] Title: 5와 6의 차이, Time: 228 ms, Memory: 18456 KB -BaekjoonHub

--- 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.java
+++ 백준/Bronze/2864. 5와 6의 차이/5와 6의 차이.java
@@ -8,40 +8,10 @@ public class Main {
 		String a = Integer.toString(sc.nextInt());
 		String b = Integer.toString(sc.nextInt());
 
-		String[] newNum = new String[4];
-		
-		for(int i=0; i<newNum.length; i++) {
-			newNum[i] = "";
-		}
-		
-		for(int i=0; i<a.length(); i++) {
-
-			if(a.charAt(i) == '5' || a.charAt(i) == '6') {
-				newNum[0] += "6";
-				newNum[1] += "5";
-			}
-			else {
-				newNum[0] += a.charAt(i)+"";
-				newNum[1] += a.charAt(i)+"";
-			}
-		}
-		
-		for(int i=0; i<b.length(); i++) {
-
-			if(b.charAt(i) == '5' || b.charAt(i) == '6') {
-				newNum[2] += '6';
-				newNum[3] += '5';
-			}
-			else {
-				newNum[2] += b.charAt(i);
-				newNum[3] += b.charAt(i);
-			}
-		}
-		
 		int max=0, min=0;
 		
-		max = Integer.parseInt(newNum[0])+Integer.parseInt(newNum[2]);
-		min = Integer.parseInt(newNum[1])+Integer.parseInt(newNum[3]);
+		max = Integer.parseInt(a.replace('5', '6'))+Integer.parseInt(b.replace('5', '6'));		// max는 5를 6으로 바꿔 더함
+		min = Integer.parseInt(a.replace('6', '5'))+Integer.parseInt(b.replace('6', '5'));		// min은 6을 5로 바꿔 더함
 		
 		System.out.println(min+" "+max);
 	
