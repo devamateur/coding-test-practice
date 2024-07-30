c5633c9d11c53c1919fac6ba5be7faf06d45a916
Mi Jeong Cho <49390382+devamateur@users.noreply.github.com>
2024-07-30 17:11:34 +0900

    [Gold IV] Title: 도시 분할 계획, Time: 3640 ms, Memory: 197612 KB -BaekjoonHub

--- 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
+++ 백준/Gold/1647. 도시 분할 계획/도시 분할 계획.py
@@ -1,38 +1,47 @@
 import sys
-input = sys.stdin.readline          ##### 
+input = sys.stdin.readline                       #### 입력
 
-N, M = map(int, input().split())
-graph = []
-parent = [i for i in range(N+1)]
+class Solution:
+    def divide(self):
 
-def find_parent(p: list, a):
-    if p[a] != a:
-        p[a] = find_parent(p, p[a])        ##### 현재 노드의 부모를 루트로 갱신
-    return p[a]
+        N, M = map(int, input().split())
+        graph = []
+        parent = [i for i in range(N+1)]
+
+        for _ in range(M):
+            # a: 집1, b: 집2, c: 유지비(cost)
+            a, b, c = map(int, input().split())
+            graph.append((c, a, b))         # cost 순으로 정렬해야 하므로 cost를 앞에 저장
+
+        # 유지비 기준 오름차순 정렬
+        graph.sort()
         
-def union(p: list, a, b):
-    a_p = find_parent(p, a)
-    b_p = find_parent(p, b)
-
-    if a_p < b_p:
-        p[b_p] = a_p
-    else:
-        p[a_p] = b_p
+        def find_parent(p: list, a):
+            if p[a] != a:
+                p[a] = find_parent(p, p[a])        #### 경로 압축, 루트를 현재 노드의 부모로 설정 설정
+            return p[a]
         
-for _ in range(M):
-    # a: 집1, b: 집2, c: 유지비(cost)
-    a, b, c = map(int, input().split())
-    graph.append((c, a, b))         # cost 순으로 정렬해야 하므로 cost를 앞에 저장
+        def union(p: list, a, b):
+            a_p = find_parent(p, a)
+            b_p = find_parent(p, b)
 
-# 유지비 기준 오름차순 정렬
-graph.sort()
+            if a_p < b_p:
+                p[b_p] = a_p
+            else:
+                p[a_p] = b_p
+
+            return p
+
+        result = 0
+        last = 0
+        for i in range(len(graph)):
+            if find_parent(parent, graph[i][1]) != find_parent(parent, graph[i][2]):
+                parent = union(parent, graph[i][1], graph[i][2])
+
+                result += graph[i][0]
+                last = graph[i][0]
         
-result = 0
-last = 0
-for i in range(len(graph)):
-    if find_parent(parent, graph[i][1]) != find_parent(parent, graph[i][2]):
-        union(parent, graph[i][1], graph[i][2])
-
-        result += graph[i][0]
-        last = graph[i][0]
-print(result - last)
+        print(result - last)
+        
+s = Solution()
+s.divide()
