675197d17f577005fe4d54302307e2bd1c5e3097
Mi Jeong Cho <49390382+devamateur@users.noreply.github.com>
2024-08-23 13:28:20 +0900

    [Silver II] Title: 병사 배치하기, Time: 388 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
+++ 백준/Silver/18353. 병사 배치하기/병사 배치하기.py
@@ -7,7 +7,7 @@ class Solution:
         dp = [1] * N
 
         for i in range(N-2, -1, -1):
-            for j in range(N-1, i-1, -1):     # j: i의 뒤에 있는 원소들
+            for j in range(N-1, i, -1):     # j: i의 뒤에 있는 원소들
                 if power[j] < power[i]:
                     dp[i] = max(dp[i], dp[j]+1)
         print(N-max(dp))
