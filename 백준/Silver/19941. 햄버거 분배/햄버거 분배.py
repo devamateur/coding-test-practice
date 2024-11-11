4901f71c92f2a02ba678ce8a18b0308087e914c7
Mi Jeong Cho <49390382+devamateur@users.noreply.github.com>
2024-11-11 13:12:13 +0900

    [Silver III] Title: 햄버거 분배, Time: 76 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
+++ 백준/Silver/19941. 햄버거 분배/햄버거 분배.py
@@ -2,21 +2,14 @@ N, K = map(int, input().split())      # N: 식탁길이, K: 햄버거 선택 거
 
 table = list(input())
 burger_count = 0
-visited = [False]*N
 
-p_list = []
 for i in range(N):
     if table[i] == 'P':
-        p_list.append(i)
-
-for i in p_list:
-    for j in range(i-K, i+K+1):
-        if j < 0 or j > N-1: continue
-        if table[j] == 'H':
-            if visited[j]:
-                continue
-            burger_count += 1
-            visited[j] = True
-            break
+        for j in range(i-K, i+K+1):
+            if j < 0 or j > N-1: continue
+            if table[j] == 'H':
+                table[j] = 0
+                burger_count += 1
+                break
 
 print(burger_count)
